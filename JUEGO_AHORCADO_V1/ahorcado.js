69b5803d3df0f24a33b492cb9f0c584ffbe271f3
belenparedes <balupards@gmail.com>
2025-10-01 21:02:13 -0500

    paso 1 corregida y funcionando

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,13 +1,49 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta="";
+guardarPalabra=function()
+{
+    let palabra=recuperarTexto("txtSecreta"); 
+    if (palabra.length==5)
+    {
+        let todasMayusculas=0;
+        for(let i=0;i<palabra.length;i++)
+        {
+        let caracters=palabra.charAt(i);
+
+         if(esMayuscula(caracters))
+        {
+            todasMayusculas++;
+        }
+        }
+        if (todasMayusculas!=5)
+        {
+            alert("Debe ingresar una palabras de 5 letras mayusculas");
+        }
+            
+        
+        else 
+        {
+            palabraSecreta=palabra;
+        }
+        
+    }  
+
+    else 
+    {
+        alert("Debe ingresar una palabras de 5 letras mayusculas");
+    }
+}
 esMayuscula=function(caracter)
 {
-    let mayuscula
-    if (caracter>=65 && caracter<=90)
+    let mayuscula=false;
+    let codigo=caracter.charCodeAt(0)
+    if (codigo>=65 && codigo<=90)
     {
-        return true;
+        mayuscula=true;
     }
-    else
+    else 
     {
-        return false;
+        mayuscula=false;
     }
+    return mayuscula;
 }
