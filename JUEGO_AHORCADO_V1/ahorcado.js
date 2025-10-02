b6b879304e6098dcf11cfe506d23a497a237709a
belenparedes <balupards@gmail.com>
2025-10-01 19:30:28 -0500

    paso 0 y paso 1 realizado

---
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -0,0 +1,13 @@
+//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+esMayuscula=function(caracter)
+{
+    let mayuscula
+    if (caracter>=65 && caracter<=90)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
