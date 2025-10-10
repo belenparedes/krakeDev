130ce82c16485ad8f6b9d6cf12ce2a3ca7ae2b23
belenparedes <balupards@gmail.com>
2025-10-09 21:20:08 -0500

    funcionamiento de la pantalla de ingreso, búsqueda y modificación de clientes

---
+++ objetos/clientes.js
@@ -0,0 +1,116 @@
+let clientes=[
+    {cedula:"123456789",nombre:"Belen",edad:20},
+    {cedula:"444444444",nombre:"Juan",edad:22},
+    {cedula:"222222222",nombre:"Pedro",edad:30},
+];
+
+guardarCambios=function()
+{
+
+    let valorCedula=recuperarTexto("txtCedula");
+    let valorNombre=recuperarTexto("txtNombre");
+    let valorEdad=recuperarFloat("txtEdad");
+    let datosCliente=
+    {   }
+    datosCliente.cedula=valorCedula;
+    datosCliente.nombre=valorNombre;
+    datosCliente.edad=valorEdad;
+    modificarCliente(datosCliente);
+    mostrarClientes();
+
+}
+
+
+modificarCliente=function(cliente)
+{
+    let clienteEncontrado=buscarCliente(cliente.cedula);
+    if(clienteEncontrado!=null)
+    {
+        clienteEncontrado.nombre=cliente.nombre;
+        clienteEncontrado.edad=cliente.edad;
+    }
+}
+
+ejecutarBusqueda=function()
+{
+    let valorCedula=recuperarTexto("txtBuscar");
+    let respuesta=buscarCliente(valorCedula);
+    if(respuesta==null)
+    {
+        alert("cliente no encontrado");
+    }else
+    {
+        mostrarTextoEnCaja("txtCedula",respuesta.cedula);
+        mostrarTextoEnCaja("txtNombre",respuesta.nombre);
+        mostrarTextoEnCaja("txtEdad",respuesta.edad);
+    }
+}
+
+crearCliente=function()
+{
+    let valorCedula=recuperarTexto("txtCedula");
+    let valorNombre=recuperarTexto("txtNombre");
+    let valorEdad=recuperarFloat("txtEdad");
+    let nuevoCliente=
+    {   }
+    nuevoCliente.cedula=valorCedula;
+    nuevoCliente.nombre=valorNombre;
+    nuevoCliente.edad=valorEdad;
+    agregarCliente(nuevoCliente);
+
+}
+agregarCliente=function(cliente)
+{
+    let resultado;
+    resultado=buscarCliente(cliente.cedula);
+if(resultado==null)
+{
+    clientes.push(cliente);
+    alert("Cliente Agregardo");
+    mostrarClientes();
+}
+else
+{
+    alert("ya existe el cliente con lacedula:"+cliente.cedula);
+}
+    
+}
+
+buscarCliente=function(cedula)
+{
+    let elementoCliente;
+    let elementoEncontrado=null;
+    for(let i=0;i<clientes.length;i++)
+    {
+        elementoCliente=clientes[i];
+        if(elementoCliente.cedula==cedula)
+        {
+            elementoEncontrado=elementoCliente;
+            break;
+        }
+        
+    }
+    return elementoEncontrado
+}
+
+
+mostrarClientes=function()
+{
+    let cmpTabla=document.getElementById("tablaClientes");
+    let contenidoTabla="<table> <tr>"+
+    "<th> CEDULA</th>"+
+    "<th> NOMBRE</th>"+
+    "<th> EDAD</th>"+
+    "</tr>";
+    let elementoCliente;
+    for(let i=0;i<clientes.length;i++)
+    {
+        elementoCliente=clientes[i];
+        contenidoTabla+="<tr><td>"+elementoCliente.cedula+"</td>"
+        +"<td>"+elementoCliente.nombre+"</td>"
+        +"<td>"+elementoCliente.edad+"</td>"
+        +"</tr>";
+    }
+    contenidoTabla+="</table>";
+    cmpTabla.innerHTML=contenidoTabla;
+}
--- objetos/objetos.js
+++ objetos/objetos.js
@@ -48,3 +48,70 @@ crearProducto=function()
         console.log("ambos productos tienen el mismo stock");
     }
 }
+modificarAtributos=function()
+{
+    let cuenta=
+    {
+        numero: "123547",
+        saldo:0.0,
+    }
+    cuenta.saldo=100;
+    cuenta.saldo+=10;
+    console.log(cuenta.saldo);
+}
+crearCliente=function()
+{
+    let cliente=
+    {
+        cedula:"1850258742",
+        nombre:"Juan"
+    }
+    let cliente1=
+    {   }
+    cliente1.nombre="Maria",
+    cliente1.apellido="Morales",
+    cliente1.cedula="12345589890"
+
+}
+probarIncrementoSaldo=function()
+{
+    let cta={
+        numero :"1234567890", saldo: 34.2
+    }
+    incrementarSaldo(cta,100);
+    console.log(cta.saldo);
+}
+probarDeterminarNayor=function()
+{
+    let per1={
+        nombre:"Belen", edad: 12
+    }
+    let per2={
+        nombre:"Maria", edad: 10
+    }
+    
+    let mayor=determinarMayor(per1,per2);
+    if(mayor!=null)
+    {
+        console.log("el mayor es"+mayor.nombre);
+    }
+}
+incrementarSaldo=function(cuenta,monto)
+{
+ cuenta.saldo+=monto;   
+}
+determinarMayor=function(persona1,persona2)
+{
+    if(persona1.edad>persona2.edad)
+    {
+        return persona1;
+    }
+    else if(persona2.edad>persona1.edad)
+    {
+        return persona2;
+    }
+    else
+    {
+        return null;
+    }
+}
