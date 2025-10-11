3b8094f0bfc2d63f4caa2a1e8b63c934b14cb8f3
belenparedes <balupards@gmail.com>
2025-10-11 10:49:54 -0500

    reto 46 hasta el paso 4 funcionando correctamente

--- 1-roles/roles/roles.js
+++ 1-roles/roles/roles.js
@@ -1,6 +1,7 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"1850287424",nombre:"Belen",apellido:"Paredes",sueldo:800.0},
 ]
 
 mostrarOpcionEmpleado=function()
@@ -8,6 +9,7 @@ mostrarOpcionEmpleado=function()
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    mostrarEmpleado();
 }
 
 mostrarOpcionRol=function()
@@ -21,4 +23,26 @@ mostrarOpcionResumen=function()
     mostrarComponente("divResumen");
     ocultarComponente("divRol");
     ocultarComponente("divEmpleado");   
+}
+mostrarEmpleado=function()
+{
+    let compTabla=document.getElementById("tablaEmpleados");
+    let contTaabla="<table>"+
+    "<tr><th>CEDULA</Th>"+
+        "<th>NOMBRE</th>"+
+        "<th>APELLIDO</th>"+
+        "<th>SUELDO</th>"+
+    "</tr>";
+    let elemento;
+    for(let i=0;i<empleados.length;i++)
+    {
+        elemento=empleados[i];
+        contTaabla+="<tr><td>"+elemento.cedula+"</td>"+
+                        "<td>"+elemento.nombre+"</td>"+
+                        "<td>"+elemento.apellido+"</td>"+
+                        "<td>"+elemento.sueldo+"</td>";
+        contTaabla+="</tr>";
+    }
+    contTaabla+="</table>";
+    compTabla.innerHTML=contTaabla;
 }
